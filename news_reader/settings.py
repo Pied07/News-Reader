817f0f3704c43f181b6ca455e123068a20c40697
Sandipan Adhikary <87486032+Pied07@users.noreply.github.com>
2024-09-22 09:30:34 +0530

    Update settings.py

--- news_reader/settings.py
+++ news_reader/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['news-reader-1.onrender.com']
+ALLOWED_HOSTS = ['news-reader-7uab.onrender.com']
 
 
 # Application definition
